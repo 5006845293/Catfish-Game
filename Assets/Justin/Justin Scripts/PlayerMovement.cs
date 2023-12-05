d47d4e3f01205ead2af7b5394c37f9bfd7418ebe
5006845293 <jasminewells@nevada.unr.edu>
2023-12-05 00:58:32 -0800

    scrollable texture to make it look like diving faster than you are

--- Assets/Justin/Justin Scripts/PlayerMovement.cs
+++ Assets/Justin/Justin Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     
     public float speed;
     private Rigidbody2D rb;
+	public Camera mainCamera;
 
     void Start()
     {
@@ -33,7 +34,22 @@ public class PlayerMovement : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0) * speed * Time.deltaTime;
+
+        // Calculate boundaries of the visible area (change according to your scene)
+        float minX = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, 0)).x;
+        float maxX = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
+        float minY = mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.0f, mainCamera.nearClipPlane)).y;
+        float maxY = mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 1.0f, mainCamera.nearClipPlane)).y;
+
+        // Move the player
+        transform.Translate(movement);
+
+        // Restrict player movement within the visible area
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, minX, maxX);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, minY, maxY);
+        transform.position = clampedPosition;
 
     }
 
