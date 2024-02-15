71fae3652e2a154e225e4df55b14d9eee028bf9f
5006845293 <jasminewells@nevada.unr.edu>
2024-02-15 08:56:56 -0800

    Revert "Merge branch 'Hook-Movement-Tania_Jaswal'"
    This reverts commit 289cf6417bcdc0f53383d014559b5235fcc44d12, reversing
    changes made to 7df915ac2b3a4869375251e4d86132b0e3b2be72.

--- Assets/3-HookGame Scene/3-Scripts/Hook.cs
+++ Assets/3-HookGame Scene/3-Scripts/Hook.cs
@@ -12,10 +12,11 @@ public class Hook : MonoBehaviour
     public Transform fishHolder;  // Assuming fishHolder is set to fishParent in the inspector
     public int maxFishCount = 5;
 	public DepthController DepthScript;
-    private bool isHooking = false;
-    private List<GameObject> fishesOnHook = new List<GameObject>();
+    public bool isHooking = true;
+    public List<GameObject> fishesOnHook = new List<GameObject>();
     private const float TopPositionY = 12.3f;
     private Vector2 hookOriginalPosition;
+
 
     // Store original positions for fishes and trash when the game starts
     private Dictionary<GameObject, Vector2> originalFishPositions = new Dictionary<GameObject, Vector2>();
@@ -33,7 +34,6 @@ public class Hook : MonoBehaviour
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         rb.interpolation = RigidbodyInterpolation2D.Interpolate;
 		isHooking = true;
-
         // Store the original position of the hook
         hookOriginalPosition = rb.position;
 
@@ -65,10 +65,11 @@ public class Hook : MonoBehaviour
 
     private void FixedUpdate()
     {
-		HandleMovementInput();
+        HandleMovementInput();
 		if(DepthScript.GetCurrentDepth()>=500){
 			isHooking = false;
 		}
+		
     }
 
     void HandleMovementInput()
@@ -76,7 +77,6 @@ public class Hook : MonoBehaviour
         Vector2 movement = new Vector2(horizontalMovement, 0.0f);
         rb.velocity = new Vector2(movement.x * movementSpeed, rb.velocity.y);
     }
-
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
@@ -103,11 +103,9 @@ public class Hook : MonoBehaviour
             fishCount += 1;
             // Update the fish count display.
             SetFishCountText();
-            Debug.Log("Fish collected: " + fish.name);
         }
         else
         {
-            Debug.Log("Reached max fish count!");
             fish.SetActive(true);
         }
     }
@@ -169,17 +167,15 @@ public class Hook : MonoBehaviour
         ActivateAllFishes();
     }
 
-   void ActivateAllFishes()
+    void ActivateAllFishes()
     {
         foreach (GameObject fish in originalFishPositions.Keys)
         {
             if (originalFishPositions.TryGetValue(fish, out Vector2 originalPosition))
             {
-                fish.transform.SetParent(fishHolder);
                 fish.SetActive(true);
                 fish.transform.position = originalPosition;
-                fishCountText.text = "Fish Count: " + fishCount.ToString();
-                Debug.Log("Activated fish: " + fish.name);
+                fish.transform.SetParent(null);
             }
         }
 
@@ -205,9 +201,10 @@ public class Hook : MonoBehaviour
     }
 
     // Function to update the displayed count of fishes collected.
-      void SetFishCountText()
+    void SetFishCountText()
     {
         fishCountText.text = "Fish Count: " + fishCount.ToString();
-        Debug.Log("Fish count updated: " + fishCount);
     }
+	
+
 }
